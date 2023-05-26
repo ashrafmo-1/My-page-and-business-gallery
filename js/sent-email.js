28eb6b1b8e43666ca5adb848961dbd9efce8a249
Ashraf Mohamed <ashrafmohamed1176@gmail.com>
2023-05-26 23:13:40 +0300

    add sent mail

--- js/sent-email.js
+++ js/sent-email.js
@@ -1,36 +1,3 @@
-// // sent email
-// const contactForm = document.getElementById('contact-form'),
-//     contactName = document.getElementById('contact-name'),
-//     contactEmail = document.getElementById('contact-email'),
-//     contactProject = document.getElementById('contact-project'),
-//     contactMessage = document.getElementById('contact-message')
-
-// const sentEmail = (e) => {
-//     e.preventDefault()
-
-//     // chick if the field has a value
-//     if (contactName.value === '' || contactEmail.value === '' || contactProject.value === '') {
-//         // add and remove color
-//         contactMessage.classList.remove('color-blue')
-//         contactMessage.classList.add('color-red')
-//         // show message
-//         contactMessage.textContent = 'Write all the input fields 🤞'
-//     } else {
-//         // service_id | template id |form | publickay
-//         emailjs.sentForm('service_mlix524' === 'template_3q4wxrp' === 'contactForm' === '6QvUOksXSN0oTjN1s')
-//             .then(() => {
-//                 contactMessage.classList.add('color-blue')
-//                 contactMessage.textContent = 'Message sent'
-
-//                 setTimeout(() =>{
-//                     contactMessage.textContent = ''
-//                 },5000)
-//             }, (error) =>{
-//             })
-//     }
-// }
-// contactForm.addEventListener('submit', sentEmail);
-
 document.getElementById('contact-form').addEventListener('submit', function (event) {
     event.preventDefault(); // Prevent the default form submission
 
