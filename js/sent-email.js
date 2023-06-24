ea081bc04e969df46e7446acf403e8d0282276f5
Ashraf Mohamed <ashrafmohamed1176@gmail.com>
2023-06-24 23:11:40 +0300

    Add message

--- js/sent-email.js
+++ js/sent-email.js
@@ -2,17 +2,18 @@ document.getElementById('contact-form').addEventListener('submit', function (eve
     event.preventDefault(); // Prevent the default form submission
 
     // Get the form values
-    var formData = {
+    const formData = {
         name: this.name.value,
         email: this.email.value,
         message: this.message.value
     };
+    const msgSent = document.querySelector(".contact-message")
 
     // Send the email using emailJS
     emailjs.send('service_mlix524', 'template_3q4wxrp', formData, '6QvUOksXSN0oTjN1s')
         .then(function (response) {
             console.log('Email sent!', response.status, response.text);
-            Swal.fire('Email sent successfully')
+            msgSent.textContent = 'Email sent successfully!';
         }, function (error) {
             console.log('Error sending email:', error);
             alert('An error occurred while sending the email.');
